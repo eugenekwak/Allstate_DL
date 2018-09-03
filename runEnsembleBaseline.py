21551c37442a010b01e8f719166d29870a40a63e
eugene <kwak113@gmail.com>
2018-09-03 08:47:38 -0400

    update run params

--- runEnsembleBaseline.py
+++ runEnsembleBaseline.py
@@ -26,13 +26,13 @@ def main():
     val_frac = 0.15
 
     # Define the parameter grid
-    param_grid = {'submodels__dtr__model__min_samples_split': [0.10],
+    param_grid = {'submodels__dtr__model__min_samples_split': [0.15],
                   'submodels__dtr__model__max_depth': [10],
-                  'submodels__dtr__model__min_samples_leaf': [0.10],
+                  'submodels__dtr__model__min_samples_leaf': [0.05],
                   'submodels__dtr__model__max_features': [10],
                   'submodels__sgd__model__learning_rate': ['optimal'],
                   'submodels__sgd__model__max_iter': [7000],
-                  'submodels__sgd__model__penalty': ['l1'],
+                  'submodels__sgd__model__penalty': ['l1', 'l2'],
                   'submodels__sgd__model__tol': [0.001],
                  }
 
